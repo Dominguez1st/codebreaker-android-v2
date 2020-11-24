80b95ffa0e98d0f72a1503b4d3ba0745ef2d2473
Robert Dominguez <dominguez.1st@gmail.com>
2020-11-24 11:11:52 -0700

    Round trip seems to work via chrome.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerWebService.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerWebService.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.codebreaker.service;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.codebreaker.BuildConfig;
+import edu.cnm.deepdive.codebreaker.model.entity.Match;
 import edu.cnm.deepdive.codebreaker.model.entity.User;
 import io.reactivex.Single;
 import okhttp3.OkHttpClient;
@@ -11,24 +12,31 @@ import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 public interface CodebreakerWebService {
 
   @GET("users/me")
   Single<User> getProfile(@Header("Authorization") String bearerToken);
-  static  CodebreakerWebService getInstance() {
+
+  @POST("matches")
+  Single<Match> startMatch(@Header("Authorization") String bearerToken, @Body Match match);
+
+  static CodebreakerWebService getInstance() {
     return InstanceHolder.INSTANCE;
   }
 
-  class InstanceHolder{
+  class InstanceHolder {
 
     private static final CodebreakerWebService INSTANCE;
 
     static {
       Gson gson = new GsonBuilder()
           .excludeFieldsWithoutExposeAnnotation()
+          .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
           .create();
       HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
       interceptor.setLevel(BuildConfig.DEBUG ? Level.BODY : Level.NONE);
